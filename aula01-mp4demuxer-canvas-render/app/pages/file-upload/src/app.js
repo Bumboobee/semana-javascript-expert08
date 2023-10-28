2d1ea79e802612fc8d6f4cc796cd256baa1c9c0d
José Aparecido <juniorsilva240803@gmail.com>
2023-10-28 16:38:58 -0300

    feature: demuxer | ler arquivo sob demanda| encode

--- aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/app.js
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/app.js
@@ -1,48 +1,54 @@
-import Clock from './deps/clock.js';
-import View from './view.js';
-const view = new View()
-const clock = new Clock()
+import Clock from "./deps/clock.js";
+import View from "./view.js";
+const view = new View();
+const clock = new Clock();
 
-const worker = new Worker('./src/worker/worker.js', { 
-    type: 'module' 
-}) 
+const worker = new Worker("./src/worker/worker.js", {
+  type: "module",
+});
 
-worker.onmessage = ({data}) => {
-    if(data.status !== 'done') return;
-    clock.stop();
-    view.updateElapsedTime(`Process took ${took.replace("ago", "")}`);
-}
+worker.onerror = (error) => {
+    console.error("deu ruim", error);
+};
 
-let took = ''
+worker.onmessage = ({ data }) => {
+  if (data.status !== "done") return;
+  clock.stop();
+  view.updateElapsedTime(`Process took ${took.replace("ago", "")}`);
+};
 
-view.configureOnFileChange(file => {
-    worker.postMessage({
-        file
-    })
+let took = "";
 
-    clock.start((time) => {
-        took = time;
-        view.updateElapsedTime(`Process started ${time}`);
-      });
-})
+view.configureOnFileChange((file) => {
+  const canvas = view.getCanvas();
+  worker.postMessage({
+    file,
+    canvas
+  }, [canvas]);
+
+  clock.start((time) => {
+    took = time;
+    view.updateElapsedTime(`Process started ${time}`);
+  });
+});
 
 async function fakeFetch() {
-    const filePath = '/videos/frag_bunny.mp4'
+  const filePath = "/videos/frag_bunny.mp4";
 
-    const response = await fetch(filePath)
-    //tamanho do arquivo
-    // const response = await fetch(filePath, {
-    //     method: 'HEAD'
-    // })
-    // response.headers.get('content-length')
-    const file = new File([await response.blob()], filePath, {
-        type: 'video/mp4',
-        lastModified: Date.now()
-    })
-    const event = new Event('change')
-    Reflect.defineProperty(event, 'target', { value: { files: [file] } })
+  const response = await fetch(filePath);
+  //tamanho do arquivo
+  // const response = await fetch(filePath, {
+  //     method: 'HEAD'
+  // })
+  // response.headers.get('content-length')
+  const file = new File([await response.blob()], filePath, {
+    type: "video/mp4",
+    lastModified: Date.now(),
+  });
+  const event = new Event("change");
+  Reflect.defineProperty(event, "target", { value: { files: [file] } });
 
-    document.getElementById("fileUpload").dispatchEvent(event);
+  document.getElementById("fileUpload").dispatchEvent(event);
 }
 
-fakeFetch()
+fakeFetch();
